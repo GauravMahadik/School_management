1d60ceed0a993650b6646f4addf77e2defe20fe3
gaurav mahadik <gauravmahadik294@gmail.com>
2026-05-15 23:09:03 +0530

    fixed deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use("/", schoolRoutes);
 
-const PORT = process.env.MYSQLPORT || 3306;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
